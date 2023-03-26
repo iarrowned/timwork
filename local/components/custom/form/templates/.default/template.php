842941cb0c8a72fc4812c432f9487c513e32b810
Кирилл <14krabik14@gmail.com>
2023-03-26 18:33:39 +0300

    Новый дизайн форм

--- local/components/custom/form/templates/.default/template.php
+++ local/components/custom/form/templates/.default/template.php
@@ -20,27 +20,42 @@ $request = Application::getInstance()->getContext()->getRequest();
 $FORM = $request->getPostList()['FORM'];
 $FORM = $this->__component->processingData($FORM);
 ?>
-<div class="task-form js-form-container">
-    <form action="" method="post" class="form my_form">
-        <input type="hidden" name="FORM[UF_USER_ID]" value="<?= $USER->GetID() ?>">
-        <input type="text" required autocomplete="off" name="FORM[UF_USER_NAME]" placeholder="Имя" value="<?= $USER->GetLastName() . ' ' . $USER->GetFirstName() ?>">
 
-        <select name="FORM[UF_STATUS]" class="form-select" disabled style="display: none">
-            <?php foreach ($arResult['STATUSES'] as $status): ?>
-                <option value="<?= $status['ID'] ?>"><?= $status['UF_NAME'] ?></option>
-            <?php endforeach; ?>
-        </select>
-        <input type="text" required name="FORM[UF_LOCATION]" placeholder="Место проведения работ (корпус, кафедра, кабинет)">
-        <input type="text" name="FORM[UF_DEPART]" required placeholder="Укажите своё подразделение (институт/кафедра/отдел)">
-        <input type="tel" required name="FORM[UF_USER_PHONE]" placeholder="Телефон">
-        <input type="email" required name="FORM[UF_USER_EMAIL]" placeholder="E-mail">
-        <textarea name="FORM[UF_USER_MESSAGE]" required cols="30" rows="10"></textarea>
-        <button class="btn" type="submit">Отправить</button>
-    </form>
-    <div class="success-from">
-        <p></p>
-        <a href="/work/list.php">Перейти в список заявок</a>
+<div class="container">
+    <div class="form_container">
+        <h1>Оставьте заявку</h1>
+        <form action="" method="post" class="my_form">
+            <input type="hidden" name="FORM[UF_USER_ID]" value="<?= $USER->GetID() ?>">
+            <select name="FORM[UF_STATUS]" class="form-select" disabled style="display: none">
+                <?php foreach ($arResult['STATUSES'] as $status): ?>
+                    <option value="<?= $status['ID'] ?>"><?= $status['UF_NAME'] ?></option>
+                <?php endforeach; ?>
+            </select>
+            <div class="input_group">
+                <input type="text" class="name_input" required autocomplete="off" name="FORM[UF_USER_NAME]" placeholder="Имя" value="<?= $USER->GetLastName() . ' ' . $USER->GetFirstName() ?>">
+            </div>
+            <div class="input_group">
+                <input type="text" class="location_input" required name="FORM[UF_LOCATION]" placeholder="Место проведения работ (корпус, кафедра, кабинет)">
+            </div>
+            <div class="input_group">
+                <input type="text" class="location_input" name="FORM[UF_DEPART]" required placeholder="Укажите своё подразделение (институт/кафедра/отдел)">
+            </div>
+            <div class="input_group">
+                <input type="tel" class="location_input" required name="FORM[UF_USER_PHONE]" placeholder="Телефон">
+            </div>
+            <div class="input_group">
+                <input type="email" class="location_input" required name="FORM[UF_USER_EMAIL]" placeholder="E-mail">
+            </div>
+            <p style="padding-top: 0">Опишите, что требуется сделать</p>
+            <div class="input_group">
+                <textarea name="FORM[UF_USER_MESSAGE]" required class="textarea_input weswap-new"></textarea>
+            </div>
+            <input class="btn submit-btn" type="submit" name="Login" value="Отправить" />
+        </form>
+        <div class="success-from">
+            <p></p>
+            <a href="/work/list.php">Перейти в список заявок</a>
+        </div>
     </div>
-
 </div>
 
--- register/index.php
+++ register/index.php
@@ -3,20 +3,30 @@
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetPageProperty('title', 'Timwork - регистрация');
 require($_SERVER["DOCUMENT_ROOT"] . "/src/Tools/HighloadTool.php");
+use Bitrix\Main\Page\Asset;
+$asset = Asset::getInstance();
+$asset->addCss('/local/components/custom/form/templates/.default/style.css');
 $departs = HighloadTool::getDeparts();
 ?>
-
-    <div class="task-form register-form__container js-form-container">
-        <h1>Запрос на регистрацию</h1>
-        <form action="/ajax/actions.php" method="post" class="form form-register">
-            <input type="hidden" name="action" value="register">
-            <input type="text" required name="UF_FIO" placeholder="Ваше ФИО">
-            <input type="email" required name="UF_EMAIL" placeholder="Ваш email">
-            <input type="tel" required name="UF_PHONE" placeholder="Ваш телефон">
-            <button class="btn" type="submit">Отправить</button>
-        </form>
-        <div class="success-from">
-            <p></p>
+    <div class="container">
+        <div class="form_container" style="height: 450px">
+            <h1>Запрос на регистрацию</h1>
+            <form action="/ajax/actions.php" method="post" class="form-register">
+                <input type="hidden" name="action" value="register">
+                <div class="input_group">
+                    <input type="text" class="name_input" required name="UF_FIO" placeholder="Ваше ФИО">
+                </div>
+                <div class="input_group">
+                    <input type="email" class="location_input" required name="UF_EMAIL" placeholder="Ваш email">
+                </div>
+                <div class="input_group">
+                    <input type="tel" required name="UF_PHONE" placeholder="Ваш телефон" class="location_input">
+                </div>
+                <input class="btn submit-btn" type="submit" name="Login" value="Отправить" />
+            </form>
+            <div class="success-from">
+                <p></p>
+            </div>
         </div>
     </div>
 <?php require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
