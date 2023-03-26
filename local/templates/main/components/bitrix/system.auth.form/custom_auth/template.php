b9997748691f2abdd3f09c377f7659cdc76e6732
Кирилл <14krabik14@gmail.com>
2023-03-26 15:48:14 +0300

    Верстка авторизации

--- local/templates/main/components/bitrix/system.auth.form/custom_auth/template.php
+++ local/templates/main/components/bitrix/system.auth.form/custom_auth/template.php
@@ -6,16 +6,14 @@ $asset->addCss(SITE_TEMPLATE_PATH . '/css/auth.css');
 ?>
 
 <div class="container">
-<div class="bx-system-auth-form auth-form__custom">
+<div class="bx-system-auth-form auth-form__custom form_container">
 
-<?php
-if ($arResult['SHOW_ERRORS'] == 'Y' && $arResult['ERROR'])
-	ShowMessage($arResult['ERROR_MESSAGE']);
-?>
+<?php if ($arResult['SHOW_ERRORS'] === 'Y' && $arResult['ERROR']) ShowMessage($arResult['ERROR_MESSAGE']); ?>
 
-<?php if($arResult["FORM_TYPE"] == "login"):?>
-            <h1 style="margin-bottom: 50px">Авторизация</h1>
-            <form class="login-form" name="system_auth_form<?=$arResult["RND"]?>" method="post" target="_top" action="<?=$arResult["AUTH_URL"]?>">
+<?php if($arResult["FORM_TYPE"] === "login"):?>
+            <img src="<?= SITE_TEMPLATE_PATH ?>/img/Portal.svg" class="top_icon" alt="">
+            <h1>Вход в систему</h1>
+            <form name="system_auth_form<?=$arResult["RND"]?>" method="post" target="_top" action="<?=$arResult["AUTH_URL"]?>">
                 <?php if($arResult["BACKURL"] <> ''):?>
                     <input type="hidden" name="backurl" value="<?=$arResult["BACKURL"]?>" />
                 <?php endif?>
@@ -24,32 +22,39 @@ if ($arResult['SHOW_ERRORS'] == 'Y' && $arResult['ERROR'])
                 <?php endforeach?>
                 <input type="hidden" name="AUTH_FORM" value="Y" />
                 <input type="hidden" name="TYPE" value="AUTH" />
-                <input type="text" placeholder="<?=GetMessage("AUTH_LOGIN")?>" name="USER_LOGIN" maxlength="50" value="" size="17" />
-                <input type="password" placeholder="<?=GetMessage("AUTH_PASSWORD")?>" name="USER_PASSWORD" maxlength="255" size="17" autocomplete="off" />
-                <div class="auth-block">
-                    <?php if ($arResult["STORE_PASSWORD"] == "Y"):?>
-                        <div class="remember">
-                            <input type="checkbox" id="USER_REMEMBER_frm" name="USER_REMEMBER" value="Y" />
-                            <label for="USER_REMEMBER_frm" title="<?=GetMessage("AUTH_REMEMBER_ME")?>"><?= GetMessage("AUTH_REMEMBER_SHORT")?></label>
-                        </div>
-                    <?php endif?>
-                    <?php if ($arResult["CAPTCHA_CODE"]):?>
-                        <?= GetMessage("AUTH_CAPTCHA_PROMT")?>:<br />
-                        <input type="hidden" name="captcha_sid" value="<?= $arResult["CAPTCHA_CODE"]?>" />
-                        <img src="/bitrix/tools/captcha.php?captcha_sid=<?= $arResult["CAPTCHA_CODE"]?>" width="180" height="40" alt="CAPTCHA" /><br /><br />
-                        <input type="text" name="captcha_word" maxlength="50" value="" />
-                    <?php endif?>
-                    <?php if($arResult["NEW_USER_REGISTRATION"] == "Y"):?>
-                        <noindex><a href="<?=$arResult["AUTH_REGISTER_URL"]?>" rel="nofollow"><?=GetMessage("AUTH_REGISTER")?></a></noindex><br />
-                    <?php endif?>
-                    <noindex><a href="<?=$arResult["AUTH_FORGOT_PASSWORD_URL"]?>" rel="nofollow"><?=GetMessage("AUTH_FORGOT_PASSWORD_2")?></a></noindex>
+                <div class="input_group">
+                    <input type="text" class="login_input" placeholder="Логин" name="USER_LOGIN" maxlength="50">
+                    <img src="<?= SITE_TEMPLATE_PATH ?>/img/person.svg" alt="">
                 </div>
-                <input class="btn submit-btn" type="submit" name="Login" value="<?=GetMessage("AUTH_LOGIN_BUTTON")?>" />
-                <a href="/register/">Регистрация</a>
+                <div class="input_group">
+                    <input type="password" class="pass_input" placeholder="Пароль" name="USER_PASSWORD" maxlength="255" autocomplete="off">
+                    <img src="<?= SITE_TEMPLATE_PATH ?>/img/visibility-off.svg" alt="" class="show_pass">
+                </div>
+                <p style="padding-top: 0">Действия</p>
+
+                <?php if ($arResult["STORE_PASSWORD"] === "Y"):?>
+                <div class="remember">
+                    <input type="checkbox" id="USER_REMEMBER_frm" name="USER_REMEMBER" value="Y" />
+                    <label for="USER_REMEMBER_frm" title="">Запомнить меня</label>
+                </div>
+                <?php endif?>
+                <?php if ($arResult["CAPTCHA_CODE"]):?>
+                    <?= GetMessage("AUTH_CAPTCHA_PROMT")?>:<br />
+                    <input type="hidden" name="captcha_sid" value="<?= $arResult["CAPTCHA_CODE"]?>" />
+                    <img src="/bitrix/tools/captcha.php?captcha_sid=<?= $arResult["CAPTCHA_CODE"]?>" width="180" height="40" alt="CAPTCHA" /><br /><br />
+                    <input type="text" name="captcha_word" maxlength="50" value="" />
+                <?php endif?>
+
+                <input class="btn submit-btn" type="submit" name="Login" value="Войти" />
+                <a href="/register/" class="register_btn">Регистрация</a>
         </form>
     <?php else:?>
-        <form action="<?=$arResult["AUTH_URL"]?>" class="logout-form">
-            <h2>Вы зашли как:</h2>
+    <?php global $USER; ?>
+        <form action="<?=$arResult["AUTH_URL"]?>">
+            <img src="<?= $USER->GetParam("PERSONAL_PHOTO") ?
+                CFile::GetPath($USER->GetParam("PERSONAL_PHOTO")) :
+                SITE_TEMPLATE_PATH . '/img/Portal.svg' ?>" class="top_icon" alt="" style="border-radius: 50%">
+            <h1>Вы зашли как:</h1>
             <div class="form-name__block">
                 <div class="form-name"><?=$arResult["USER_NAME"]?></div>
                 <div class="form-name"><?=$arResult["USER_LOGIN"]?></div>
@@ -61,11 +66,22 @@ if ($arResult['SHOW_ERRORS'] == 'Y' && $arResult['ERROR'])
             <?php endforeach?>
             <?=bitrix_sessid_post()?>
             <input type="hidden" name="logout" value="yes" />
-            <div class="btn-block">
-                <input type="submit" class="btn logout-btn" name="logout_butt" value="<?=GetMessage("AUTH_LOGOUT_BUTTON")?>" />
-            </div>
+            <input class="btn submit-btn" type="submit" name="logout_butt" value="<?=GetMessage("AUTH_LOGOUT_BUTTON")?>" />
         </form>
     <?php endif?>
-
+    <script>
+        const showPass = document.querySelector('.show_pass');
+        if(showPass) {
+            showPass.addEventListener('click', (e) => {
+                e.preventDefault();
+                const parent = e.target.parentNode.querySelector('.pass_input');
+                if (parent.getAttribute('type') === 'password') {
+                    parent.setAttribute('type', 'text');
+                } else {
+                    parent.setAttribute('type', 'password');
+                }
+            });
+        }
+    </script>
 </div>
 </div>
